a81e41184759df0b6ec7fcc95a3a8151b0fb11e6
milanthomson784-web <milanthomson784@gmail.com>
2026-04-13 10:49:22 +0530

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    
+    @Test
+    public void testEligibility() {
+        App app = new App();
+        app.addSubject("Math", 10);
+        app.addSubject("Science", 6);
+        
+     
+        assertEquals(16, app.getTotalCredits());
+        assertTrue(app.isEligible());
+    }
+
+    @Test
+    public void testNotEligible() {
+        App app = new App();
+        app.addSubject("History", 5);
+        
+        
+        assertFalse(app.isEligible());
+    }
+}
